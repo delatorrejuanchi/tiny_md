8e24979095c70c246df5cd4e93f44a1ba285e09c
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2023-05-14 19:25:53 -0300

    fix test script

--- test/test_main.py
+++ test/test_main.py
@@ -20,8 +20,32 @@ DEFAULT_SUCCESS_THRESHOLD = 0.9
 
 
 def run_tiny_md(N: int) -> str:
+    subprocess.run(
+        ["rm", ".depend"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
+    subprocess.run(
+        ["make", "clean"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
+    subprocess.run(
+        ["make", f'USER_DEFINES="-DN={N}"'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
     result = subprocess.run(
-        ["./tiny_md", str(N)],
+        ["./tiny_md"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
@@ -61,7 +85,8 @@ def test(N: int, num_runs: int, success_threshold: float) -> None:
         expected_output = parse_output(f.read())
 
     with open(os.path.join("test", f"expected_stats_{N}.txt"), "r") as f:
-        mean_epsilon_values, std_epsilon_values = ([float(x) for x in line.split()] for line in f.read().split("\n"))
+        mean_epsilon_values, std_epsilon_values = (
+            [float(x) for x in line.split()] for line in f.read().split("\n"))
 
     successful_runs = 0
     for i in range(num_runs):
@@ -97,7 +122,8 @@ def test(N: int, num_runs: int, success_threshold: float) -> None:
 
 
 def compute_statistics(N: int, num_runs: int) -> None:
-    print(f"Computing statistics for tiny_md with {N} particles after {num_runs} runs...")
+    print(
+        f"Computing statistics for tiny_md with {N} particles after {num_runs} runs...")
 
     outputs = []
     for i in range(num_runs):
@@ -125,8 +151,10 @@ def compute_statistics(N: int, num_runs: int) -> None:
         print("Writing expected statistics to file...", end=" ")
 
         # Set the epsilon values to 2 standard deviations from the mean, which should cover ~95% of the cases.
-        mean_epsilon_values = np.mean(mean_diffs, axis=0) + 2 * np.std(mean_diffs, axis=0)
-        std_epsilon_values = np.mean(std_diffs, axis=0) + 2 * np.std(std_diffs, axis=0)
+        mean_epsilon_values = np.mean(
+            mean_diffs, axis=0) + 2 * np.std(mean_diffs, axis=0)
+        std_epsilon_values = np.mean(
+            std_diffs, axis=0) + 2 * np.std(std_diffs, axis=0)
 
         f.write(" ".join([f"{x:.6f}" for x in mean_epsilon_values]))
         f.write("\n")
