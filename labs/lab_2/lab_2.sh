050029f22d1660557871321bd69b04cba2e676a6
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2023-05-15 00:28:10 -0300

    lab2

--- labs/lab_2/lab_2.sh
+++ labs/lab_2/lab_2.sh
@@ -16,8 +16,20 @@ git checkout main
 make
 make name=baseline benchmark
 
-# lab 2 vectorization
+# lab 2 vectorization (AoS)
 git checkout lab2-vectorization
-make name=vectorization benchmark
+make name=vectorization_aos benchmark
+
+# lab 2 vectorization - float (AoS)
+git checkout lab2-vectorization-float
+make name=vectorization_aos_float benchmark
+
+# lab 2 vectorization (SoA)
+git checkout lab2-vectorization-soa
+make name=vectorization_soa benchmark
+
+# lab 2 vectorization - float (SoA)
+git checkout lab2-vectorization-soa-float
+make name=vectorization_soa_float benchmark
 
 make name=lab2 plot
